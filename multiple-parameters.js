1aae4c816b9f49b2d9de6f851b732acb1ddebb31
Naimur Rahman <naimur2231@gmail.com>
2023-01-29 23:41:37 +0600

    multiple parameters js

---
+++ multiple-parameters.js
@@ -0,0 +1,12 @@
+function addition(num1, num2, num3){
+    var sum = num1 + num2 + num3;
+    console.log(sum);
+}
+
+function subtraction(num1, num2){
+    var sub = num1 - num2;
+    console.log(sub);
+}
+
+addition(5, 10, 5);
+subtraction(30, 20);
