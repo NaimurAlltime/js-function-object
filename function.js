5ecd9cc6f38e672434715c305ab65b327ebaff24
Naimur Rahman <naimur2231@gmail.com>
2023-01-29 23:21:50 +0600

    Function in js

---
+++ function.js
@@ -0,0 +1,11 @@
+// function declaration 
+function exercise(){
+    console.log("First vor bela ghum theke uthbo");
+    console.log("Tarpor namaz porbo");
+    console.log("Then Workout Korbo");
+} 
+
+// function calling 
+exercise();
+console.log("Protidin akoi kaj korbo");
+exercise();
