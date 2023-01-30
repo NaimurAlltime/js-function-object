02d6525e6674af97482e8f4e7ba4f02bdb7bccae
Naimur Rahman <naimur2231@gmail.com>
2023-01-30 22:29:44 +0600

    object in js

---
+++ object.js
@@ -0,0 +1,16 @@
+var student = {
+    name: "Naimur Rahman",
+    age: 22,
+    studying: "BSc in CSE"
+}
+console.log(student);
+console.log(student.studying);
+
+var myComputer = {
+    name: "HP",
+    processor: "intel cor-i5",
+    price: '42,200'
+}
+
+console.log(myComputer);
+console.log(myComputer.price);
